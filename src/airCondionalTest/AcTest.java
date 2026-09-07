092a3f84dca90fe2d53af7efc02cd993608c06d4
Rozarioehiz <rozarioehiz@gmail.com>
2026-09-07 07:52:17 +0100

    tested if my Ac is on

--- src/airCondionalTest/AcTest.java
+++ src/airCondionalTest/AcTest.java
@@ -1,4 +1,25 @@
 package airCondionalTest;
 
+
+import airConditional.Ac;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class AcTest {
+
+    @Test
+    public void testThatAcIsOff(){
+        Ac ac = new Ac();
+        assertFalse(ac.isOn);
+
+    }
+    @Test
+    public void testThatAcIsOn(){
+        Ac ac = new Ac();
+        ac.turnOn();
+        assertTrue(ac.isOn);
+
+    }
 }
--- src/airConditional/Ac.java
+++ src/airConditional/Ac.java
@@ -1,4 +1,16 @@
 package airConditional;
 
 public class Ac {
+
+    public boolean isOn;
+    public boolean isOff;
+
+    public static boolean isOn(){
+        return isOn();
+    }
+
+    public void turnOn() {
+        isOn = true;
+    }
+
 }
